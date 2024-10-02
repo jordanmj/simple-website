90089325e10d1708e242cfaa65d233aabcdc6e5c
Jordan M James <jordanmjames3@gmail.com>
2024-10-02 10:05:17 +0200

    initial push

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('loadData').addEventListener('click', function() {
+    const contentDiv = document.getElementById('content');
+    contentDiv.innerHTML = 'Here is some loaded data!';
+});
+
